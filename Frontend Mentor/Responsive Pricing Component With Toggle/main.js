c9232955724300bbcab39aa2c927f776ebe5be18
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-12-06 13:57:00 +0530

    formatting the docs

--- Frontend Mentor/Responsive Pricing Component With Toggle/main.js
+++ Frontend Mentor/Responsive Pricing Component With Toggle/main.js
@@ -1,13 +1,13 @@
-let checkBox = document.querySelector('#toggle');
-let Monthly = document.querySelectorAll('.price-monthly');
-let Yearly = document.querySelectorAll('.price-annually');
+let checkBox = document.querySelector("#toggle");
+let Monthly = document.querySelectorAll(".price-monthly");
+let Yearly = document.querySelectorAll(".price-annually");
 
-checkBox.addEventListener('change', function(){
-    if(checkBox.checked){
-         Yearly.forEach(price => price.style.display = 'none');
-         Monthly.forEach(price => price.style.display = 'flex');
-    } else {
-       Yearly.forEach(price => price.style.display = 'flex');
-         Monthly.forEach(price => price.style.display = 'none');
-    }
+checkBox.addEventListener("change", function () {
+  if (checkBox.checked) {
+    Yearly.forEach((price) => (price.style.display = "none"));
+    Monthly.forEach((price) => (price.style.display = "flex"));
+  } else {
+    Yearly.forEach((price) => (price.style.display = "flex"));
+    Monthly.forEach((price) => (price.style.display = "none"));
+  }
 });
