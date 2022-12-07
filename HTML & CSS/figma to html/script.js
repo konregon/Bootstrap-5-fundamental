1c859e344288f5fa9c458eb5ccda8c6aeafdda5c
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-12-07 12:15:41 +0530

    js file

---
+++ HTML & CSS/figma to html/script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.nav');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+
+
+open.addEventListener('click', () => {
+    nav.style.display = 'inline-block';
+    open.classList.add('hidden');
+    close.classList.remove('hidden');
+})
+close.addEventListener('click', () => {
+    nav.style.display = 'none';
+    open.classList.remove('hidden');
+    close.classList.add('hidden');
+})
